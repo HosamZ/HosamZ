5fcef81e3dded44c92145aae3917ff7da30abf53
HosamZ <hsmzaror@gmail.com>
2019-01-11 17:31:50 +0100

    cleaned my code with L way

--- src/evaluation/week8/ThermostatTest.java
+++ src/evaluation/week8/ThermostatTest.java
@@ -10,28 +10,34 @@ class ThermostatTest {
     @Test
     void testSkiing() {
         Double temperature = 2.0;
-        Double temperature2 = -1.0;
 
         String actual = thermostat.sense(temperature);
         String expected = temperature + "degrees celsius";
+
+        Assertions.assertEquals(expected, actual);
+
+        Double temperature2 = -1.0;
+
         String actual2 = thermostat.sense(temperature2);
         String expected2 = "Warning!";
 
-        Assertions.assertEquals(expected, actual);
         Assertions.assertEquals(expected2, actual2);
     }
 
     @Test
     void testSauna() {
         Double temperature = 79.0;
-        Double temperature2 = 80.0;
 
-        String actual = thermostat2.sense(temperature);
-        String actual2 = thermostat2.sense(temperature2);
         String expected = temperature + 273.15 + "degrees Kelvin";
-        String expected2 = "Warning!";
+        String actual = thermostat2.sense(temperature);
 
         Assertions.assertEquals(expected, actual);
+
+        Double temperature2 = 80.0;
+
+        String expected2 = "Warning!";
+        String actual2 = thermostat2.sense(temperature2);
+
         Assertions.assertEquals(expected2, actual2);
     }
 }
