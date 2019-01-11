0c2750f9b186483a303395ed20d9c81fa36aa399
HosamZ <hsmzaror@gmail.com>
2019-01-11 14:17:18 +0100

    finished ThermostatSaunaTest

--- src/evaluation/week8/ThermostatTest.java
+++ src/evaluation/week8/ThermostatTest.java
@@ -7,31 +7,40 @@ class ThermostatTest {
 
     @Test
     void testSkiing() {
-        Thermostat thermostat = new Thermostat(s->s<0,t->t+"degrees celsius");
+        Thermostat thermostat = new Thermostat(s -> s < 0, t -> t + "degrees celsius");
 
         Double temperatur = 2.0;
         String actual = thermostat.sense(temperatur);
-        String expected = temperatur+"degrees celsius";
+        String expected = temperatur + "degrees celsius";
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void testWarningSkiing() {
-        Thermostat thermostat = new Thermostat(s->s<0,t->t+"degrees celsius");
+        Thermostat thermostat = new Thermostat(s -> s < 0, t -> t + "degrees celsius");
 
         Double temperatur = -1.0;
         String actual = thermostat.sense(temperatur);
-        String expected = temperatur+"Warning!";
+        String expected = temperatur + "Warning!";
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void testSauna() {
-        Thermostat thermostat = new Thermostat(s->s>=80,t->t+"degrees Kelvin");
+        Thermostat thermostat = new Thermostat(s -> s >= 80, t -> t+273.15+ "degrees Kelvin");
+        Thermostat thermostat2 = new Thermostat(s -> s >= 80, t -> t+273.15+ "degrees Kelvin");
 
-        Double temperatur = 80.0;
+        Double temperatur = 79.0;
+        Double temperatur2 = 80.0;
+        String actual = thermostat.sense(temperatur);
+        String actual2 = thermostat.sense(temperatur2);
+        String expected = temperatur+273.15+"degrees Kelvin";
+        String expected2 = "Warning!";
 
+        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected2,actual2);
 
     }
 }
