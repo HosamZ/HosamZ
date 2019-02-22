fda031a98fa7d98956854168725497781f634538
HosamZ <hsmzaror@gmail.com>
2019-02-22 14:53:38 +0100

    removed unneeded empty Lines

--- src/evaluation/fruitshop/FruitShopApp.java
+++ src/evaluation/fruitshop/FruitShopApp.java
@@ -1,12 +1,7 @@
 package evaluation.fruitshop;
 
-import evaluation.fruitshop.model.Fruit;
 import evaluation.fruitshop.view.FruitCounter;
-import lombok.extern.java.Log;
 
-import java.util.List;
-
-@Log
 public class FruitShopApp {
     public static void main(String[] args) {
         FruitCounter.display();
